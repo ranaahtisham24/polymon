815c0c96993f0dd08cf71da49b9da7ba30bc0796
ranaahtisham24 <76520043+ranaahtisham24@users.noreply.github.com>
2021-12-19 17:53:15 +0500

    basic structure

---
+++ pages/openpack.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const openpack = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default openpack
---
+++ pages/polymon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const polymon = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default polymon
