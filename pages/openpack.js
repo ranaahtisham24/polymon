86bc5efbf8d7b703c06a91fd3ed0216f4077400f
ranaahtisham24 <76520043+ranaahtisham24@users.noreply.github.com>
2021-12-19 20:55:47 +0500

    openpack

--- pages/openpack.js
+++ pages/openpack.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../components/header/Header'
 import OpenPackHero from '../components/openpack/OpenPackHero'
 
 const openpack = () => {
     return (
         <div className='openpack-wrapper'>
+            <Header/>
             <OpenPackHero/>
         </div>
     )
