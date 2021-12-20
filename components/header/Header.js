950b2c2a6a2cf52102caf39413050bd2c297c238
MAwaisme <muhammad.awais.genius@gmail.com>
2021-12-20 18:54:05 +0500

    ad modal

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/assets/images/logo.svg";
+import Popup from "./Popup";
 
-const Header = ({polymon, store, openpack}) => {
+
+const Header = ({ polymon, store, openpack }) => {
+  const [modalShow, setModalShow] = useState(false);
   return (
-    <Navbar className={polymon? "polymon-header header" : 'header' || openpack? 'openpack-header header': 'header' || store? 'store-header header' : 'header'}>
+    <Navbar
+      className={
+        polymon
+          ? "polymon-header header"
+          : "header" || openpack
+          ? "openpack-header header"
+          : "header" || store
+          ? "store-header header"
+          : "header"
+      }
+    >
       <Navbar.Brand href="#home">
         <Image
-        //   loader={myLoader}
+          //   loader={myLoader}
           src={logo}
           alt="Picture of the author"
           width={150}
@@ -31,7 +44,12 @@ const Header = ({polymon, store, openpack}) => {
           <Link href="/polymon">
             <a>Polymon</a>
           </Link>
-          <button className="contact">Connect</button>
+          <button className="contact" onClick={() => setModalShow(true)}>Connect</button>
+          {/* <Button variant="primary" onClick={() => setModalShow(true)}>
+            Connect
+          </Button> */}
+
+          <Popup show={modalShow} onHide={() => setModalShow(false)} />
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
